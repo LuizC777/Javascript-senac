763ac20ab0961a810020df03cdf3eabde529cae3
Luiz Claro <luiz.claro.lima@gmail.com>
2024-08-03 10:15:20 -0300

    Inicio da aula 2

---
+++ Aula02/js/script.js
@@ -0,0 +1,7 @@
+
+// criamos a variavel cor para guardar a cor que o usuario ira digitar, usamos tambem, o comando chamado prompt que abre uma caixa
+// de mensagem para o usuario digitar a cor 
+cor = prompt("digite uma cor para o fundo em ingles")
+
+// a cor digitada pelo usuario ficou na variav el cor e sera usada para alterar a cor de fundo do body do nosso documento
+document.body.style.backgroundColor = cor
