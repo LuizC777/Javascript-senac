0685a0eea8eb94402c9752583ce14958fa556584
Luiz Claro <luiz.claro.lima@gmail.com>
2024-08-17 12:00:34 -0300

    feito começo do sistema de seleção de cores da paleta

---
+++ aula04/js/script2.js
@@ -0,0 +1,13 @@
+let cores="<table>"
+for(let i = 0; i<=10; i++){
+    cores+="<tr>"
+    for(let p = 0; p<= 50; p++){
+        let r = parseInt(Math.random()*255)
+        let g = parseInt(Math.random()*255)
+        let b = parseInt(Math.random()*255)
+        cores+= "<td style=background-color:rgb("+r+","+g+","+b+")> </td>"
+    }
+    cores+="</tr>"
+}
+cores+="</table>"
+document.getElementById("paleta").innerHTML= cores
