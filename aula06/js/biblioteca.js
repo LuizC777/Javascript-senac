36263a65ad26f8b59ca386dcb500c70e2cef4c8d
Luiz Claro <luiz.claro.lima@gmail.com>
2024-08-31 11:59:01 -0300

    trabalho com functions de soma e média

---
+++ aula06/js/biblioteca.js
@@ -0,0 +1,15 @@
+function somaArray(array){
+    let rs = 0
+    for (let i = 0; i < array.length; i++ ){
+        rs+= array[i]
+    }
+    return rs
+}
+
+function mediaArray(array){
+    let rs = 0
+    for (let i = 0; i < array.length; i++ ){
+        rs+= array[i]
+    }
+    return rs / array.length
+}
